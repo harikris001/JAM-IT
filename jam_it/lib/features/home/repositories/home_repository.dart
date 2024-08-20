86fec3359ae476a799d9dd373325b90b3401f1f5
Harikrishna R <hk5983835@gmail.com>
2024-08-20 19:20:53 +0530

    home repository provider

---
+++ jam_it/lib/features/home/repositories/home_repository.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:http/http.dart' as http;
+import 'package:jam_it/core/constants/server_constants.dart';
+import 'package:jam_it/core/failure/failure.dart';
+import 'package:jam_it/features/home/model/song_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'home_repository.g.dart';
+
+@riverpod
+HomeRepository homeRepository(HomeRepositoryRef ref) {
+  return HomeRepository();
+}
+
+class HomeRepository {
+  Future<Either<AppFailure, String>> uploadSong({
+    required File audio,
+    required File image,
+    required String songName,
+    required String artist,
+    required String hexCode,
+    required String token,
+  }) async {
+    try {
+      final request = http.MultipartRequest(
+        'POST',
+        Uri.parse('${ServerConstants.serverUrl}/song/upload'),
+      );
+
+      request
+        ..files.addAll([
+          await http.MultipartFile.fromPath('song', audio.path),
+          await http.MultipartFile.fromPath('thumbnail', image.path),
+        ])
+        ..fields.addAll(
+            {'artist': artist, 'song_name': songName, 'hex_code': hexCode})
+        ..headers.addAll({'x-auth-token': token});
+
+      final res = await request.send();
+
+      if (res.statusCode != 201) {
+        return Left(AppFailure(message: await res.stream.bytesToString()));
+      }
+
+      return Right(await res.stream.bytesToString());
+    } catch (e) {
+      return Left(AppFailure(message: e.toString()));
+    }
+  }
+
+  Future<Either<AppFailure, List<SongModel>>> getAllSongs({
+    required String token,
+  }) async {
+    try {
+      final res = await http
+          .get(Uri.parse('${ServerConstants.serverUrl}/song/list'), headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': token,
+      });
+
+      var resBodyMap = jsonDecode(res.body);
+      if (res.statusCode != 200) {
+        resBodyMap = resBodyMap as Map<String, dynamic>;
+        return Left(AppFailure(message: resBodyMap['detail']));
+      }
+
+      resBodyMap = resBodyMap as List;
+      List<SongModel> songs = [];
+
+      for (final map in resBodyMap) {
+        songs.add(SongModel.fromMap(map));
+      }
+
+      return Right(songs);
+    } catch (e) {
+      return Left(AppFailure(message: e.toString()));
+    }
+  }
+}
