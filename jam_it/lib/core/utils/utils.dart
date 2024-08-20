bfc608279c6771279e4e8e24147fc39563090118
Harikrishna R <hk5983835@gmail.com>
2024-08-20 19:26:04 +0530

    utility helper functions

--- jam_it/lib/core/utils/utils.dart
+++ jam_it/lib/core/utils/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void snackBarPopUp(BuildContext context, String content) {
@@ -7,4 +10,38 @@ void snackBarPopUp(BuildContext context, String content) {
       content: Text(content),
     ),
   );
+}
+
+Future<File?> pickAudio() async {
+  try {
+    final fileResutPath =
+        await FilePicker.platform.pickFiles(type: FileType.audio);
+    if (fileResutPath != null) {
+      return File(fileResutPath.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<File?> pickImage() async {
+  try {
+    final fileResutPath =
+        await FilePicker.platform.pickFiles(type: FileType.image);
+    if (fileResutPath != null) {
+      return File(fileResutPath.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+String rgbToHex(Color color) {
+  return '${color.red.toRadixString(16).padLeft(2, '0')}${color.green.toRadixString(16).padLeft(2, '0')}${color.blue.toRadixString(16).padLeft(2, '0')}';
+}
+
+Color hexToColor(String hex) {
+  return Color(int.parse(hex, radix: 16) + 0xFF000000);
 }
