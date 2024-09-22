1090f055c8c2f83a1039677a785033e18fe460b2
Harikrishna R <hk5983835@gmail.com>
2024-09-22 21:13:16 +0530

    backend linking playlist route

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import auth, song
+from routes import auth, song, playlist
 from database import engine
 from models.base import Base 
 
@@ -7,5 +7,6 @@ app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(song.router, prefix='/song')
+app.include_router(playlist.router, prefix='/playlist')
 
 Base.metadata.create_all(engine)
