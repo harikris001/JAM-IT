1ceeceda11bb0e79290f5da6d3d74e74610a8e00
Harikrishna R <hk5983835@gmail.com>
2024-11-13 10:55:30 +0530

    server head request

--- server/routes/server_check.py
+++ server/routes/server_check.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get('/', status_code=200)
 def server_check():
     return {"message": "Server is running"}
+
+@router.head('/', status_code=200)
+def head_check():
+    return {"message": "Server is running"}
